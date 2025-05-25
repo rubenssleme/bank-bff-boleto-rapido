361a6b39c757732059e4baae7691c8db73b1a9c6
Rubens Leme <rubenss.leme@gmail.com>
2025-05-25 09:33:06 -0300

    feat:(add initial project structure with configuration files and error handling classes)

---
+++ src/main/java/br/com/bank/leme/bff/rapid/payment/adapter/input/boleto/BoletoRapidoController.java
@@ -0,0 +1,12 @@
+package br.com.bank.capd.bff.rapid.payment.adapter.input.boleto;
+
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1")
+@Validated
+public class BoletoRapidoController {
+
+}
---
+++ src/main/java/br/com/bank/leme/bff/rapid/payment/adapter/monitoring/entity/HealthCheckServDepend.java
@@ -0,0 +1,9 @@
+package br.com.bank.capd.bff.rapid.payment.adapter.monitoring.entity;
+
+import org.springframework.boot.actuate.health.Health;
+
+/**
+ * Health Check customizado especifico para identificar a saude dos servicos dependentes
+ */
+public record HealthCheckServDepend(String url, Health health) {
+}
