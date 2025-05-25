5a0f0353a2a3d410e4a5802a3295f83d6cc3dde8
Rubens Leme <rubenss.leme@gmail.com>
2025-05-25 08:52:03 -0300

    feat:(add initial project structure with configuration files and error handling classes)

--- src/main/java/br/com/bradesco/capd/bff/rapid/payment/adapter/monitoring/entity/HealthCheckServDepend.java
+++
@@ -1,9 +0,0 @@
-package br.com.bank.capd.bff.rapid.payment.adapter.monitoring.entity;
-
-import org.springframework.boot.actuate.health.Health;
-
-/**
- * Health Check customizado especifico para identificar a saude dos servicos dependentes
- */
-public record HealthCheckServDepend(String url, Health health) {
-}
